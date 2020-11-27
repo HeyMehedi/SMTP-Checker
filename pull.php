e4344b8b4982725674c179cc4cf57db0694f8b5d
Md Mehedi Hasan <43073560+HeyMehedi@users.noreply.github.com>
2020-11-27 06:58:27 +0600

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,7 +1,9 @@
 <?php
 
+echo ;
+
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec( ‘cd /home/admin/web/pingsmtp.heymehedi.com/public_html/ && git reset –hard HEAD && git pull’ );
+shell_exec( "cd ".__DIR__." && git reset –hard HEAD && git pull" );
 }
