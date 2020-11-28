bd56c8556450aa11799a1ed99298f247d91db5b0
root <root@host.heymehedi.com>
2020-11-28 08:55:34 -0600

    sanitized the code

--- smtp.php
+++ smtp.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -8,23 +9,31 @@ require 'vendor/autoload.php';
 
 $hostname = '';
 $port = '';
+$secure = '';
 $username = '';
 $password = '';
-
 $recipient = '';
 $subject = '';
 $bodytext = '';
 
 if (isset($_POST['submit'])) {
+    $hostname = filter_input(INPUT_POST, 'hostname', FILTER_SANITIZE_STRING);
+    $port = filter_input(INPUT_POST, 'port', FILTER_SANITIZE_STRING);
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $recipient = filter_input(INPUT_POST, 'recipient', FILTER_SANITIZE_STRING);
+    $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+    $bodytext = filter_input(INPUT_POST, 'bodytext', FILTER_SANITIZE_STRING);
+    
+    $secure = filter_input(INPUT_POST, 'secure', FILTER_SANITIZE_STRING);
+    if ("tls" == $secure) {
+       $secure = "PHPMailer::ENCRYPTION_STARTTLS";
+    }elseif("ssl" == $secure){
+       $secure = "PHPMailer::ENCRYPTION_SMTPS";
+    }
 
-    $hostname = $_POST['hostname'];
-    $port = $_POST['port'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    
 
-    $recipient = $_POST['recipient'];
-    $subject = $_POST['subject'];
-    $bodytext = $_POST['bodytext'];
 
 
     // Instantiation and passing `true` enables exceptions
@@ -38,7 +47,7 @@ if (isset($_POST['submit'])) {
         $mail->SMTPAuth = true; // Enable SMTP authentication
         $mail->Username = "$username"; // SMTP username
         $mail->Password = "$password"; // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->SMTPSecure = "$secure"; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port = "$port"; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
@@ -55,7 +64,7 @@ if (isset($_POST['submit'])) {
         $mail->send();
         header('location: /?status=success');
     } catch (Exception $e) {
-        echo "<pre>Message could not be sent. Mailer Error: {$mail->ErrorInfo}</pre>";
+        echo "<pre> Message could not be sent. Mailer Error: {$mail->ErrorInfo} </pre>";
 
     }
 }
